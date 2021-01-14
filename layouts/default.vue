3029c48fdf844fcfb036de6a45ca8fc854c0e0bf
Eduardo Ferreira <eduardo.ckf@gmail.com>
2021-01-14 07:04:33 -0300

    feat: get user data

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,3 +4,19 @@
         <nuxt />
     </div>
 </template>
+
+<script>
+export default {
+	created() {
+		this.getUserInfo();
+	},
+	methods: {
+		async getUserInfo() {
+			const response = await fetch("/.auth/me");
+			const payload = await response.json();
+			const { clientPrincipal } = payload;
+			this.$store.commit('user', clientPrincipal);
+		}
+	}
+}
+</script>
