fd2d3c2a0556372de9596591434cbfc5b8d4da12
Eduardo Ferreira <eduardo.ckf@gmail.com>
2021-01-14 07:21:54 -0300

    feat: devtools true for deploid

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,43 @@
 
 export default {
-  target: 'static',
-  components: true,
+	target: 'static',
+	components: true,
 
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
 
-  loading: { color: '#fff' },
+	vue: {
+		config: {
+			devtools: true
+		}
+	},
 
-  css: [
-  ],
+	loading: { color: '#fff' },
 
-  plugins: [
-  ],
+	css: [
+	],
 
-  buildModules: [
-    '@nuxtjs/tailwindcss'
-  ],
+	plugins: [
+	],
 
-  modules: [
-  ],
+	buildModules: [
+		'@nuxtjs/tailwindcss'
+	],
 
-  build: {
-    extend (config, ctx) {
-    }
-  }
+	modules: [
+	],
+
+	build: {
+		extend (config, ctx) {
+		}
+	}
 }
