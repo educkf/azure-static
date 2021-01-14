ff2fff648bc772ffd0152b6dbb9f22e0ff1ae5e1
Eduardo Ferreira <eduardo.ckf@gmail.com>
2021-01-14 08:01:29 -0300

    feat: better no-access page

---
+++ pages/no-permission.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-5xl font-bold title">
+      Ops
+    </h1>
+	<p>You have no access to that page...</p>
+	<p v-if="!user">Maybe if you <a href="/login">Sign in</a>?</p>
+  </div>
+</template>
+
+<script>
+export default {
+	computed: {
+		user() {
+			return this.$store.state.user;
+		}
+	}
+}
+</script>
